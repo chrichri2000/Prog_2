1e52d518da6f38fbfb2945820b2bbaf756c171ce
chrichri2000 <christian.musumeci.main@gmail.com>
2021-05-24 15:05:55 +0200

    Aggiunti Ins e Canc

--- New.cpp
+++ New.cpp
@@ -15,6 +15,43 @@ class ArrayDinamico
             arr = new int[dim];
         }
 
+        void set_dim(int dim)
+        {
+            this->dim = dim;
+        }
+
+        int get_dim()
+        {
+            return dim;
+        }
+
+        void ins(int x)
+        {
+            if(n == dim)
+            {
+                dim*=2;
+                int *new_arr = new int[dim];
+                for(int i=0;i<n;i++) new_arr[i] = arr[i];
+                int *to_delete = arr;
+                delete [] to_delete;
+                return;
+            }
+            arr[n++] = x;
+        }
+
+        void canc(int i)
+        {
+            if(n < 0) return;
+            if(i == n)
+            {
+                n--;
+                return;
+            }
+            arr[i] = arr[n-1];
+            arr[n-1] = 0;
+            n--;
+        }
+
         void print()
         {
             cout << "Funziona" << endl;
@@ -24,6 +61,10 @@ class ArrayDinamico
 
 int main()
 {
-    ArrayDinamico a(10);
+    int n = 10;
+    ArrayDinamico a(n);
+    for(int i=0;i<n;i++) a.ins(i);
+    a.print();
+    a.canc(10);
     a.print();
 }
