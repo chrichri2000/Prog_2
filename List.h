2b156bbcc09c7884990e56ab74537fd5f62664ae
chrichri2000 <christian.musumeci.main@gmail.com>
2021-05-31 17:02:07 +0200

    Final Working Commit

--- List.h
+++ List.h
@@ -1,63 +1,136 @@
 #include "Node.h"
+using namespace std;
 #ifndef LIST_H
 #define LIST_H
+
 template <class T> class List
 {
     private:
-        int n,len;
-        Nodo<T> *head;
-    
+        Node<T> *head, *tail;
+        int n,ascend;
+
     public:
-        List(int n,int len,Nodo<T> *head) : n(n), len(len), head(head) {}
+        List(int n, int ascend) : n(n), ascend(ascend), head(NULL), tail(NULL) {}
+        List() : List(0,1) {}
 
-        List() : List(0,10,NULL) {}
+        void setAscend(int ascend) { this->ascend = ascend; }
+        int getAscend() { return this->ascend; }
 
-        void Ins(Nodo<T> *x) //Inserimento In Coda
+        void Ins_Head( T x )
         {
-            if(n >= len) return;
+            Node<T> *new_node = new Node<T>(x);
             n++;
-            if(head == NULL)
-            {
-                head = x;
-                return;
+
+            if(head == NULL && tail == NULL) 
+            { 
+                head = new_node;
+                tail = new_node;
+                return; 
             }
-            Nodo<T>* current = head;
-            while(current->getNext() != NULL)
-            {
-                current = current->getNext();
-            }
-            current->setNext(x);
-            x->setNext(NULL);
+                new_node->setNext(head);
+                head->setPrev(new_node);
+                head = new_node;
         }
 
-        void Ins(T value)
+        void Ins_Tail( T x )
         {
-            if(n >= len) return;
+            Node<T> *new_node = new Node<T>(x);
             n++;
-            if(head == NULL)
+
+            if(head == NULL && tail == NULL)
             {
-                head->setValue(value);
+                tail = head = new_node;
                 return;
             }
-            Nodo<T>* new_node = new Nodo<T>(value);
-            Nodo<T>* current = head;
-            while(current->getNext() != NULL)
-            {
-                current = current->getNext();
-            }
-            current->setNext(new_node);
-            new_node->setNext(NULL);
+            new_node->setPrev(tail);
+            tail->setNext(new_node);
+            tail = new_node;
         }
 
-        void stampa()
+        void Ins(T x)
         {
-            if(head == NULL) return;
-            Nodo<T> *current = head;
-            while(current!=NULL)
+            Node<T> *new_nodo = new Node<T>(x);
+            n++;
+
+            if(head == NULL && tail == NULL)
             {
-                cout << current->getValue() << endl;
+                head = tail = new_nodo;
+            }
+            
+            Node<T> *tmp = head;
+            if(ascend == 1)
+            {
+                while(tmp && tmp->getValue() < new_nodo->getValue()) { tmp = tmp->getNext(); }
+                new_nodo->setNext(tmp->getNext());
+                new_nodo->setPrev(tmp);
+                tmp->setNext(new_nodo);
+                new_nodo->getNext()->setPrev(new_nodo);
+                return;
+            }
+
+            else
+            {
+                while(tmp && tmp->getValue() > new_nodo->getValue()) { tmp = tmp->getNext(); }
+                new_nodo->setNext(tmp->getNext());
+                new_nodo->setPrev(tmp);
+                tmp->setNext(new_nodo);
+                new_nodo->getNext()->setPrev(new_nodo);
+                return;
+            }
+        }
+
+        void Canc( T value )
+        {
+            if(head == NULL ) return;
+            Node<T> *current = head;
+            while(current && current->getValue() != value)
+            {
                 current = current->getNext();
             }
+            if(!current) return;
+            n--;
+            if(head == current)
+            {
+                head = current->getNext();
+                head->setPrev(NULL);
+                delete current;
+                return;
+            }
+
+            if(tail == current)
+            {
+                tail = current->getPrev();
+                tail->setNext(NULL);
+                delete current;
+                return;
+            }
+            current->getPrev()->setNext(current->getNext());
+            current->getNext()->setPrev(current->getPrev());
+        }
+
+        string to_String() const
+        {
+            stringstream ss;
+            if( head == NULL ) return "Nothing to show";
+            Node<T> *current = head;
+            while( current!=NULL )
+            {
+                current = current->getNext();
+                ss << current->getValue() << " ";
+            }
+            return ss.str();
+        }
+
+        friend ostream &operator <<(ostream &os, const List<T> &l)
+        {
+            if(l.head == NULL ) return os;
+            Node<T> *current = l.head;
+            while(current != NULL) 
+            { 
+                os << current->getValue() << " "; 
+                current = current->getNext();
+            }
+            return os;
         }
 };
 #endif
--- Node.h
+++ Node.h
@@ -1,25 +1,73 @@
 #include <iostream>
+#include <sstream>
+using namespace std;
 #ifndef NODE_H
 #define NODE_H
-using namespace std;
 
-template <class T> class Nodo
+template <class T> class Node
 {
     private:
         T value;
-        Nodo<T> *next;
+        Node<T> *next, *prev;
+
+        int compare(const Node<T> &n) const
+        {
+            if(this->value == n.value()) return 0;
+            if(this->value < n.value()) return -1;
+            if(this->value > n.value()) return 1;
+            return 0;
+        }
     
     public:
-
-        Nodo(T value, Nodo<T> *next) : value(value), next(next) {}
-
-        Nodo() : Nodo(0,NULL){}
-
-        Nodo(T value) : Nodo(value,NULL){}
+        Node(T value, Node<T> *next, Node<T> *prev) : value(value), next(next), prev(prev) {}
+        Node(T x) : Node(x,NULL,NULL) {}
+        Node() : Node(0,NULL,NULL) {}
 
         void setValue(T value) { this->value = value; }
-        T getValue() { return value; }
-        void setNext(Nodo<T> *next) { this->next = next; }
-        Nodo<T> *getNext() { return next; }
+        void setNext(Node<T> *next) { this->next = next; }
+        void setPrev(Node<T> *prev) { this->prev = prev; }
+
+        T getValue() const { return this->value; }
+        Node<T> *getNext() const { return this->next; }
+        Node<T> *getPrev() const { return this->prev; }
+
+        string to_String()
+        {
+            stringstream ss;
+            ss << "Valore del nodo: " << value << "successivo: " << next << "precedente: " << prev << "."<< endl;
+            return ss.str();
+        }
+
+        friend ostream &operator <<(ostream &os, const Node<T> &n)
+        {
+            os << n.to_String() << endl;
+            return os;
+        }
+
+        bool operator <(Node<T> &n)
+        {
+            return this->compare(n) < 0;
+        }
+
+        bool operator >(Node<T> &n)
+        {
+            return this->compare(n) > 0;
+        }
+
+        bool operator <=(Node<T> &n)
+        {
+            return this->compare(n) <= 0;
+        }
+
+        bool operator >=(Node<T> &n)
+        {
+            return this->compare(n) >= 0;
+        }
+
+        bool operator ==(Node<T> &n)
+        {
+            return this->compare(n) == 0;
+        }
+
 };
 #endif
